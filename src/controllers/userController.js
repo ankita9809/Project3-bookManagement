0d511d30c134c461a9df5bdf42e9f4d83ab31ecd
ankita9809 <k.ankita037@gmail.com>
2022-07-06 12:52:44 +0530

    User Controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,20 @@
 const userModel = require("../models/userModel")
+
+
+// ----------------------- CREATE USER -------------------
+
+const createUser= async function(req, res){
+    try{
+        const userData = req.body
+
+        let savedData = await userModel.create(userData)
+        return res.status(201).send({status: true, message: "Success", data: savedData })
+
+    } catch(error){
+        res.status(500).send({status: false, message: error.message})
+    }
+    
+
+}
+
+module.exports.createUser = createUser
--- src/routes/route.js
+++ src/routes/route.js
@@ -11,4 +11,7 @@ const mid = require("../middleware/auth")
 
 
 
+
+
+
 module.exports = router;
