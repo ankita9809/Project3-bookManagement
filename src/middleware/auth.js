f1a163e5d90d87b60561092ade2e52ac7981a1c5
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-07-06 13:12:07 +0530

    Middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+
+const auth = function(req, res){
+    try {
+        let token = req.headers["x-api-key"]
+        if(!token)  return res.status(401).send({status : false, message : "Please provide a token"})
+
+        jwt.verify(token, "CACA", (err, decodedToken) => {
+            if(err) return res.status(401).send({status : false, message : "Incorrect token"})
+            req.token = decodedToken
+            next()
+        })
+    } catch (error) {
+        return res.status(500).send({status : false, message : error.message})
+    }
+}
+
+module.exports = {auth}
