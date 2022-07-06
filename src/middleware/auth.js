86191eb91ef3b24784f1450ad9c807743fa133eb
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-07-06 17:04:02 +0530

    token expiry

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,13 +6,8 @@ const auth = function(req, res, next){
         if(!token)  return res.status(401).send({status : false, message : "Please provide a token"})
 
         jwt.verify(token, "CACA", (err, decodedToken) => {
-            if(err) return res.status(401).send({status : false, message : "Incorrect token",error : err })
-
-            if (Date.now() > (decodedToken.exp) * 1000) { 
-
-                return res.status(401).send({ status: false, message: "Session expired! Please login again." })
-            
-            }
+            if(err && err.message == "jwt expired")    return res.status(401).send({ status: false, message: "Session expired! Please login again." })
+            if(err) return res.status(401).send({status : false, message : "Incorrect token"})
 
             req.token = decodedToken
             next()
