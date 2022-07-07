5f505b678df2a4182b6d86dd43c5646027ea2b99
ranaarjunktd <ranaarjunktd@gmail.com>
2022-07-07 13:29:30 +0530

    validator

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -17,12 +17,8 @@ const moment =  require('moment')
 }
    
 
-<<<<<<< HEAD
-module.exports = { isValid, isValidRequestBody, isValidPassword,} 
-=======
 const isValidDate = function(date) {
     return moment(date, 'YYYY-MM-DD', true).isValid()
 }
 
 module.exports = { isValid, isValidRequestBody, isValidPassword, isValidDate} 
->>>>>>> ec4bd0eb9db9fb6b8ac03ad6fb3b384335de4a26
