581ff423de2b7d0c38c5b1ce1b4288851847b587
chetang12 <102719631+chetang12@users.noreply.github.com>
2022-07-07 11:18:10 +0530

    chetan

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -1,6 +1,10 @@
 const booksModel = require("../models/booksModel")
+const userModel = require("../models/userModel")
 const validator = require('../validator/validator')
+const validateDate = require("validate-date");
 
+// --------------------------- REGEX -----------------------------
+const stringRegex = /^[ a-z ]+$/i
 // ------------------------------- CREATE BOOKS ----------------------------------------------------------
 
 const bookCreation = async function (req, res) {
@@ -12,12 +16,9 @@ const bookCreation = async function (req, res) {
             return res.status(400).send({ status: false, message: 'Invalid request parameters. Please provide book details' })
         }
         if (!validator.isValid(title)) {
-            return res.status(400).send({ status: false, message: "Title must be present" })
+            return res.status(400).send({ status: false, message: "Title   must be present" })
         };
-        let checkBook = await booksModel.find({title})
-        if(checkBook){
-            return res.status(400).send({ status: false, message: "Title already used" })
-        }
+        
         if (!validator.isValid(excerpt)) {
             return res.status(400).send({ status: false, message: "excerpt must be present" })
         };
@@ -29,7 +30,7 @@ const bookCreation = async function (req, res) {
             return res.status(400).send({status: false,msg: "Incorrect userId format"})
         } 
         //Authentication
-        if (userId != req.userId) {
+        if (userId !=  req.userId ) {
             return res.status(403).send({
                 status: false,
                 message: "Unauthorized access ! User's credentials do not match."
@@ -38,16 +39,37 @@ const bookCreation = async function (req, res) {
         if (!validator.isValid(ISBN)) {
             return res.status(400).send({ status: false, message: "ISBN must be present" })
         };
-        let checkBook2 = await booksModel.find({ISBN})
-        if(checkBook2){
-            return res.status(400).send({ status: false, message: "ISBN already used" })
-        }
+      
         if (!validator.isValid(category)) {
             return res.status(400).send({ status: false, message: "category must be present" })
+        };
+        if (!category.match(stringRegex)) {
+            return res.status(400).send({ status: false, msg: "category cannot be number" })
         };
         if (!validator.isValid(subcategory)) {
             return res.status(400).send({ status: false, message: "subcategory must be present" })
         };
+        if (!subcategory.match(stringRegex)) {
+            return res.status(400).send({ status: false, msg: "subcategory cannot be number" })
+        };
+        if (!validator.isValid(releasedAt)) {
+            return res.status(400).send({ status: false, message: "releasedAt must be present" })
+        };
+        if (!validateDate(releasedAt, responseType = 'boolean')) {
+            return res.status(400).send({ status: false, message: `Invalid date format. Please provide date as 'YYYY-MM-DD'.` })
+        }
+         //searching title & ISBN in database to maintain their uniqueness.
+          let checkBook = await booksModel.findOne({ title: title })
+        if(checkBook){
+            return res.status(400).send({ status: false, message: "Title already used" })
+        }
+         let checkBook2 = await booksModel.findOne({ ISBN: ISBN})
+        if(checkBook2){
+            return res.status(400).send({ status: false, message: "ISBN already used" })
+        }
+    
+        const newBook = await booksModel.create(requestBody);
+        return res.status(201).send({ status: true, message: "Book created successfully", data: newBook })
     } catch (error) {
         res.status(500).send({ status: false, message: error.message })
     }
